b2e6978526bc9e04b5a5a188252bf1f95044ae62
mchtyener <mcht.yener@gmail.com>
2024-03-08 11:47:21 +0300

    lazy-loading tekniği tamamlandı.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+
+    {
+        path: 'home',
+        loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)
+    },
+
+    {
+        path: 'auth',
+        loadChildren: () => import('./components/auth/auth.routes').then(m => m.authRoutes)
+    },
+
+];
